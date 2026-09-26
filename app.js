e4c53bdefc70b0e02d190259921eabe673740544
Webburner <152245396+RyanrealAF@users.noreply.github.com>
2026-09-25 23:40:22 -0700

    Redesign Seuss as a reader-first theory library

---
+++ app.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',()=>{const target=document.querySelector(a.getAttribute('href'));if(target)target.scrollIntoView({behavior:'smooth'})}));
